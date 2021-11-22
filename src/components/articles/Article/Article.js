524a17ee8ef997480e4cf508653f872222505c38
Leweyse <darylcastro67@gmail.com>
2021-11-22 14:32:25 +0100

    initial

--- src/components/articles/Article/Article.js
+++ src/components/articles/Article/Article.js
@@ -1,7 +1,11 @@
 const Article = (props) => {
     return (
+
         <article>
             <img src={props.src} alt={props.alt} />
+            <p onClick={() =>
+                document.documentElement.style.setProperty('--clr-black', 'green')
+            }>Hello</p>
         </article>
     )
 }
--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -2,9 +2,10 @@ import { Article } from "../../components";
 import img from '../../assets/standing.gif';
 
 function HomePage(props) {
+    
+
     return (
         <section className="home">
-
             <Article src={img} />
         </section>
     )
