84cd4b0765c6560a6fde424a497e5668b1badb29
Leweyse <darylcastro67@gmail.com>
2021-11-26 17:14:17 +0100

    fixed github profiles

--- src/components/containers/Nav/Nav.js
+++ src/components/containers/Nav/Nav.js
@@ -4,9 +4,9 @@ const Nav = () => {
     return(
         <nav>
             <Link id={"sven"} href={"https://github.com/sven-i-am"} content={"Sven"}/>
-            <Link id={"stefan"} href={"https://github.com/sven-i-am"} content={"Stefan"}/>
-            <Link id={"daryl"} href={"https://github.com/sven-i-am"} content={"Daryl"}/>
-            <Link id={"jorg"} href={"https://github.com/sven-i-am"} content={"Jörg"}/>
+            <Link id={"stefan"} href={"https://github.com/StefanAmur"} content={"Stefan"}/>
+            <Link id={"daryl"} href={"https://github.com/Leweyse"} content={"Daryl"}/>
+            <Link id={"jorg"} href={"https://github.com/jvondzerza"} content={"Jörg"}/>
         </nav>
     )
 }
