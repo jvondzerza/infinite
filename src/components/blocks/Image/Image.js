f6779796137bfb3060c75912f68f452496e7552e
Sven <sven.vander.mierde@gmail.com>
2021-11-26 15:34:47 +0100

    switched to useRef

--- src/components/blocks/Image/Image.js
+++ src/components/blocks/Image/Image.js
@@ -1,7 +1,7 @@
 const Image = (props) => {
     return (
         <div className={"img"}>
-            <img src={props.url} alt={props.description} className={props.className} id={props.id}/>
+            <img ref={props.refference} src={props.url} alt={props.description} className={props.className} id={props.id}/>
             <figcaption>Image by {props.credit} on <a href={props.creditUrl}>Unsplash</a></figcaption>
         </div>
     )
