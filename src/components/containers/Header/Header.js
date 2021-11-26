bc5d14ca508641d60711e87575dc5b2eb6efbe42
jvondzerza <jvondzerza@googlemail.com>
2021-11-26 13:59:08 +0100

    bouncing blob

--- src/components/containers/Header/Header.js
+++ src/components/containers/Header/Header.js
@@ -6,6 +6,7 @@ const Header = () => {
     let timelineCircle = gsap.timeline({paused: false});
     let timelineParallelogram = gsap.timeline({paused:false, repeat: -1});
     let timelineTriangle = gsap.timeline({paused: false, repeat: -1});
+    let timelineBlob = gsap.timeline({paused: false, repeat: -1})
 
     window.addEventListener("load", () => {
         timelineSquare.fromTo('#square', {rotation: 10, y: -50}, {rotation: -5, y: 10, duration: 4, delay: 0.5, yoyo: true, yoyoEase: "in", repeat: -1});
@@ -16,10 +17,13 @@ const Header = () => {
             .fromTo("#parallelogram", {y: 10, rotation: -5}, {duration: 3.5, rotation: 10, y: -20, ease: "in"})
             .to('#parallelogram', {rotation: 0, duration: 3, ease: "in"});
         timelineTriangle
-            .fromTo("#triangle", {rotation: 0, y: 0}, {duration: 3, rotation: -15, y: 10, ease: "in"})
-            .to('#triangle', {duration: 3, rotation: 0, y: 0, ease: "in"})
-            .fromTo("#triangle", {rotation: 0, y: 0}, {duration: 2, rotation: -15, y: 80, ease: "inOut"})
-            .to('#triangle', {duration: 4, delay: 1, rotation: 0, y: 0, ease: "out"});
+            .fromTo("#triangle", {rotation: 0, y: 0}, {delay: 0.5, duration: 3, rotation: -15, y: 10, ease: "in"})
+            .to('#triangle', {duration: 2, rotation: 0, y: 0, ease: "in"})
+            .fromTo("#triangle", {rotation: 0, y: 0}, {delay: 0.5, duration: 1.5, rotation: -15, y: 80, ease: "out"})
+            .to('#triangle', {duration: 2, delay: 1, rotation: 0, y: 0, ease: "out"});
+        timelineBlob
+            .fromTo("#blob", {y: 300, rotation: 180}, {y: -300, duration: 4, rotation: 0, ease: "bounce.out"})
+            .fromTo("#blob", {y: -300, rotation: 0}, {y: 300, rotation: 180, duration: 4, ease: "bounce.out"})
     })
 
     return(
@@ -29,6 +33,7 @@ const Header = () => {
             <Svg shape={'triangle'} className={'svgAbsolute svgBackground svgMedium'} fillColor={'purple'} id={'triangle'} />
             <Svg shape={'parallelogram'} className={'svgAbsolute svgBackground svgMedium'} fillColor={'salmon'} id={'parallelogram'} />
             <Svg shape={'circle'} className={'svgAbsolute svgBackground svgMedium'} fillColor={'teal'} id={'circle'} />
+            <Svg shape={'blob'} className={'svgAbsolute svgBackground svgMedium'} fillColor={'gold'} id={'blob'} />
             <Title id={"header_title"} content={"Welcome to the Infinite Scroll"} />
             <Text id={"header_text"} text={"Scroll down and explore our templates"} />
         </header>
